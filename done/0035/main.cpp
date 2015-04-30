668ec904259f8c2eda60a135102642d5e82e98ae
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-04-29 21:20:16 -0600

    Fix warnings in problem 35.

--- done/0035/main.cpp
+++ done/0035/main.cpp
@@ -18,6 +18,7 @@
 
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <map>
 
@@ -30,19 +31,17 @@
 
 /*
  * The number, 197, is called a circular prime because all rotations of the
- *digits:
- * 197, 971, and 719, are themselves prime.
+ * digits: 197, 971, and 719, are themselves prime.
  *
- * There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71,
- *73,
- * 79, and 97.
+ * There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37,
+ * 71, 73, 79, and 97.
  *
  * How many circular primes are there below one million?
  */
 
 int main(void)
 {
-	int count;
+	uint32_t count;
 	ECircularPrimeSieve s;
 
 	s.setLimit(PRIME_LIMIT);
