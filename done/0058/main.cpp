a07083cf0dc8a9bdc4142921d35c2891287be83a
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-04-29 22:54:26 -0600

    Fix warnings in problem 58.

--- done/0058/main.cpp
+++ done/0058/main.cpp
@@ -27,17 +27,14 @@
 #include "libeuler/euler/ESpiral.h"
 
 /*
- * This value is sufficiently high enough for us to get the correct answer, and
- * sufficiently low enough
- * to achieve our goal of executing in < 1second on the netbook -- do not
- * increase it!
+ * This value is sufficiently high enough for us to get the correct answer,
+ * and sufficiently low enough to find the answer very quickly.
  */
 #define PRIME_PRECISION 25
 
 /*
  * Starting with 1 and spiralling anticlockwise in the following way, a square
- *spiral with side
- * length 7 is formed.
+ * spiral with side length 7 is formed.
  *
  *    (37)36 35 34 33 32(31)
  *     38(17)16 15 14(13)30
@@ -48,16 +45,13 @@
  *    (43)44 45 46 47 48 49
  *
  * It is interesting to note that the odd squares lie along the bottom right
- *diagonal, but what is
- * more interesting is that 8 out of the 13 numbers lying along both diagonals
- *are prime; that is, a
- * ratio of 8/13 = 62%.
+ * diagonal, but what is more interesting is that 8 out of the 13 numbers lying
+ * along both diagonals are prime; that is, a ratio of 8/13 = 62%.
  *
- * If one complete new layer is wrapped around the spiral above, a square spiral
- *with side length 9
- * will be formed. If this process is continued, what is the side length of the
- *square spiral for which
- * the ratio of primes along both diagonals first falls below 10%?
+ * If one complete new layer is wrapped around the spiral above, a square
+ * spiral with side length 9 will be formed. If this process is continued, what
+ * is the side length of the square spiral for which the ratio of primes along
+ * both diagonals first falls below 10%?
  */
 
 int main(void)
