81dc1f6c2b7d921491f5b5d08e131d7870453d0e
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-08-26 00:11:17 -0700

    Add a completed Rust port of the solution to problem 96.
    This solution drops a lot of the custom sudoku reduction and threading
    code the old problem had, but it turns out the new Vec-based exact cover
    implementation is fast enough that we don't need any of it. As-is, this
    solution is not even close to being one of the top-ten slowest problems.
    So, good enough.

--- src/bin/00096.rs
+++ src/bin/00096.rs
@@ -60,136 +60,16 @@ const EXPECTED_RESULT: u64 = 24702;
 
 fn main() {
     main_impl(|| -> Result<ProblemAnswer<u64>> {
-        let puzzles = Puzzle::from_text(INPUT_PUZZLES)?;
-
         Ok(ProblemAnswer {
-            actual: 0,
+            actual: Puzzle::from_text(INPUT_PUZZLES)?
+                .into_iter()
+                .map(|mut puzzle| {
+                    puzzle.solve();
+                    let solution = puzzle.get_solution().unwrap();
+                    solution[0][0] as u64 * 100 + solution[1][0] as u64 * 10 + solution[2][0] as u64
+                })
+                .sum(),
             expected: EXPECTED_RESULT,
         })
     });
 }
-
-/*
-#include <algorithm>
-#include <atomic>
-#include <cstddef>
-#include <fstream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <system_error>
-#include <thread>
-#include <vector>
-
-#include "common/euler/ESudoku.h"
-#include "common/util/Path.hpp"
-#include "common/util/Process.hpp"
-
- * This structure defines the context of a consumer thread, including our list
- * of puzzles that are to be solved, the total from the solved puzzles, and an
- * error flag for consumers to indicate failure.
-typedef struct ThreadContext
-{
-	std::vector<std::vector<int>> puzzles;
-	std::atomic<std::size_t> next;
-	std::atomic<int> total;
-
-	bool error;
-
-} ThreadContext;
-
- * Consumers take unsolved puzzles, solve them, and then add the resulting
- * answer to our context.
-void solvePuzzles(ThreadContext &context)
-{
-	ESudoku solver;
-	int a;
-	std::vector<int> puzzle;
-
-	// Keep going until the loop terminates itself.
-
-	while(true)
-	{
-		// Get the next puzzle and increment the index.
-		std::size_t p =
-		        context.next.fetch_add(1, std::memory_order_seq_cst);
-		if(static_cast<std::size_t>(p) >= context.puzzles.size())
-			break;
-
-		// Try to solve the puzzle.
-
-		if(!solver.load(context.puzzles.at(p)))
-		{
-			std::cout << "Failed to load puzzle!\n";
-			context.error = true;
-			return;
-		}
-
-		if(!solver.solve(true))
-		{
-			std::cout << "Failed to solve puzzle!\n";
-			context.error = true;
-			return;
-		}
-
-		puzzle = solver.getSolution();
-
-		a = puzzle[0];
-		a *= 10;
-		a += puzzle[1];
-		a *= 10;
-		a += puzzle[2];
-
-		// Add our solution to the total.
-		context.total.fetch_add(a, std::memory_order_seq_cst);
-	}
-}
-
-euler::util::process::ProblemResult<int> problem()
-{
-	std::vector<int> puzzle;
-
-	// Initialize thread context.
-
-	ThreadContext context;
-	context.next = 0;
-	context.total = 0;
-	context.error = false;
-
-	// Spin up some threads to solve the puzzles.
-	std::vector<std::shared_ptr<std::thread>> threads;
-	for(unsigned i = 0; i < std::thread::hardware_concurrency(); ++i)
-	{
-		auto work = [&context]() { solvePuzzles(context); };
-		threads.push_back(std::make_shared<std::thread>(work));
-	}
-
-	// Wait for the threads to finish up.
-	for(auto thread : threads)
-	{
-		try
-		{
-			thread->join();
-		}
-		catch(const std::system_error &e)
-		{
-			if(e.code() != std::errc::invalid_argument)
-				throw;
-		}
-	}
-	threads.clear();
-
-	// Print the answer!
-
-	if(!context.error)
-	{
-		return {context.total.load(std::memory_order_seq_cst),
-		        EXPECTED_RESULT};
-	}
-	else
-	{
-		throw std::runtime_error(
-		        "One or more threads did not complete successfully!");
-	}
-}
-*/
--- src/dsc/sudoku.rs
+++ src/dsc/sudoku.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use math::exact_cover::{Coordinate, ExactCover};
 use util::error::*;
 
 const PUZZLE_SIZE: usize = 9;
@@ -152,10 +153,121 @@ fn parse_puzzle_values(text: &str) -> Result<Vec<(String, PuzzleValues)>> {
     Ok(puzzles)
 }
 
+/// This function constructs a new ExactCover which can be used to represent
+/// the given problem. This simply returns an ExactCover of the appropriate
+/// size; it is up to the caller to then fill in the "1 nodes" in the returned
+/// structure so it can be used to solve the problem.
+fn construct_exact_cover(values: &PuzzleValues) -> ExactCover {
+    let cols: usize = 324;
+    let mut rows: usize = 0;
+
+    for x in 0..PUZZLE_SIZE {
+        for y in 0..PUZZLE_SIZE {
+            if values[x][y] == 0 {
+                rows += 9;
+            } else {
+                rows += 1;
+            }
+        }
+    }
+
+    ExactCover::new(cols, rows)
+}
+
+/// Returns the nth box, which contains the given cell in a puzzle. This
+/// function doesn't do bounds checking, so the caller must be sure to pass
+/// only valid coordinates.
+fn box_of(x: usize, y: usize) -> usize {
+    let i = y * PUZZLE_SIZE + x;
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    const LOOKUP_TABLE: &'static [usize] = &[
+        0, 0, 0,   1, 1, 1,   2, 2, 2,
+        0, 0, 0,   1, 1, 1,   2, 2, 2,
+        0, 0, 0,   1, 1, 1,   2, 2, 2,
+
+        3, 3, 3,   4, 4, 4,   5, 5, 5,
+        3, 3, 3,   4, 4, 4,   5, 5, 5,
+        3, 3, 3,   4, 4, 4,   5, 5, 5,
+
+        6, 6, 6,   7, 7, 7,   8, 8, 8,
+        6, 6, 6,   7, 7, 7,   8, 8, 8,
+        6, 6, 6,   7, 7, 7,   8, 8, 8,
+    ];
+    LOOKUP_TABLE[i]
+}
+
+/// This function fills in "1 nodes" in the given ExactCover structure (which
+/// is assumed to be of the right size), to represent the given sudoku puzzle
+/// state.
+///
+/// A bit about how the problem is represented:
+///
+/// For cells which are filled in, we can express them in a single row. For
+/// cells that are not filled in, we need to express them as nine different
+/// rows (one for each possible value), to express that there are nine possible
+/// solutions for that cell.
+///
+/// For each row, we have 324 columns. That's 4 * 81, or one set of 81 columns
+/// per the cell, row, column, and box in the puzzle (respectively). So for
+/// each cell in the puzzle, we put a "1 node" at:
+///
+/// 1. (i, row) where the cell is the ith = (y * PUZZLE_SIZE + x)th cell in the
+/// puzzle, to indicate that the ith cell has a value in it.
+/// 2. 81 + ((row * 9) + value - 1), to indicate that the rowth row has that
+/// particular value in it.
+/// 3. 162 + ((col * 9) + value - 1), to indicate that the colth column has
+/// that particular value in it.
+/// 4. 243 + ((box * 9) + value - 1), to indicate that the boxth box has that
+/// particular value in it.
+///
+/// For cells which aren't filled in, we follow a similar process as the above,
+/// but we do it *once per value, 1-9*.
+fn populate_exact_cover(exact_cover: &mut ExactCover, values: &PuzzleValues) {
+    let mut row: usize = 0;
+    for y in 0..PUZZLE_SIZE {
+        for x in 0..PUZZLE_SIZE {
+            let i = (y * PUZZLE_SIZE) + x; // The cell at (x, y) is the ith cell.
+
+            if values[x][y] == 0 {
+                // This is a zero cell, so we need to setup 9 rows for it. For each possible
+                // value, 1-9...
+                for value in 1..10 {
+                    exact_cover.set(i, row, true).unwrap();
+                    exact_cover
+                        .set(81 + (y * 9) + value - 1, row, true)
+                        .unwrap();
+                    exact_cover
+                        .set(162 + (x * 9) + value - 1, row, true)
+                        .unwrap();
+                    exact_cover
+                        .set(243 + (box_of(x, y) * 9) + value - 1, row, true)
+                        .unwrap();
+                    row += 1;
+                }
+            } else {
+                // This is a filled-in cell, so we only need a single row for it.
+                let value = values[x][y] as usize;
+                exact_cover.set(i, row, true);
+                exact_cover
+                    .set(81 + (y * 9) + value - 1, row, true)
+                    .unwrap();
+                exact_cover
+                    .set(162 + (x * 9) + value - 1, row, true)
+                    .unwrap();
+                exact_cover
+                    .set(243 + (box_of(x, y) * 9) + value - 1, row, true)
+                    .unwrap();
+                row += 1;
+            }
+        }
+    }
+}
+
 pub struct Puzzle {
     name: String,
     values: PuzzleValues,
     states: PuzzleDigitStates,
+    solution: Option<PuzzleValues>,
 }
 
 impl Puzzle {
@@ -165,6 +277,7 @@ impl Puzzle {
             name: name,
             values: values,
             states: states,
+            solution: None,
         })
     }
 
@@ -208,5 +321,81 @@ impl Puzzle {
     /// None is returned instead.
     pub fn get_single_digit(&self, x: usize, y: usize) -> Option<u8> {
         self.states.0[x][y].get_single_digit()
+    }
+
+    /// This function returns the previously-computed full solution to this
+    /// puzzle. It is possible that a valid puzzle will have multiple possible
+    /// solutions: in this case, only the first solution that was found is
+    /// returned. Note that this function will return None until solve() has
+    /// been called.
+    pub fn get_solution(&self) -> Option<&PuzzleValues> { self.solution.as_ref() }
+
+    /// This function takes the input puzzle, and reduces the problem as much
+    /// as is worthwhile using simple logic. This might result in a solution,
+    /// but it also might only simplify the problem.
+    ///
+    /// NOTE: Currently, no extra problem reduction / simplification is
+    /// implemented, because this structure is already "fast enough" relying
+    /// solely on ExactCover. But, this is an easy place to add more
+    /// optimization if it is needed in the future.
+    fn reduce(&self) -> PuzzleValues { self.values.clone() }
+
+    /// This function computes a full solution for the puzzle. This function
+    /// must be called before get_solution() will return anything.
+    pub fn solve(&mut self) {
+        if self.solution.is_some() {
+            return;
+        }
+
+        // Represent the puzzle as an exact cover problem, and get a solution for it.
+        let values = self.reduce();
+        let mut exact_cover = construct_exact_cover(&values);
+        populate_exact_cover(&mut exact_cover, &values);
+        exact_cover.solve(Some(1));
+        // In the solution, we want to discard any node coordinates which are in a
+        // column >= 81. These nodes are used to represent the one-value-per-{row,
+        // column, box} constraints, but they are not useful to construct a solution
+        // Puzzle.
+        let solution: Option<Vec<Coordinate>> =
+            exact_cover.get_solutions().get(0).map(|solution| {
+                solution
+                    .iter()
+                    .filter(|coord| coord.col < 81)
+                    .cloned()
+                    .collect()
+            });
+        if solution.is_none() {
+            return;
+        }
+        let mut solution = solution.unwrap();
+
+        // Translate the exact cover solution to a set of puzzle values. The column of
+        // the solution node is in the range of [0, 81), and it indicates that the node
+        // represents the columnth value in the puzzle. So, this needs to be converted
+        // to an (x, y) coordinate in the puzzle. The row of the solution node
+        // indicates in some sense the selected solution value. Recall that when we
+        // constructed the ExactCover, any filled-in cells in the puzzle produced 1
+        // row, and any 0 cells produced 9 rows. The solution contains exactly one node
+        // from each of these 1- or 9-row "groups". For filled-in cells we can just
+        // copy the value, but for others the selected value is the offset from the
+        // start of the 9-row group. So, we need to iterate over the solution nodes in
+        // *column-order* (or, in puzzle cell order), and keep track of the start of
+        // the row group.
+        solution.sort_by_key(|coord| coord.col);
+        let mut solution_values: PuzzleValues = [[0; PUZZLE_SIZE]; PUZZLE_SIZE];
+        let mut from_row: usize = 0;
+        for coord in solution {
+            let x = coord.col % PUZZLE_SIZE;
+            let y = coord.col / PUZZLE_SIZE;
+
+            if values[x][y] != 0 {
+                solution_values[x][y] = values[x][y];
+                from_row += 1;
+            } else {
+                solution_values[x][y] = (coord.row + 1 - from_row) as u8;
+                from_row += 9;
+            }
+        }
+        self.solution = Some(solution_values);
     }
 }
